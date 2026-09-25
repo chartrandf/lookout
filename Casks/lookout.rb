4d6bc6f15c31b62d051539d378184543116d8de7
Francis Chartrand <chartrandf@users.noreply.github.com>
2026-09-25 14:20:22 -0400

    chore: release v0.4.0

--- Casks/lookout.rb
+++ Casks/lookout.rb
@@ -1,5 +1,5 @@
 cask "lookout" do
-  version "0.3.6"
+  version "0.4.0"
   sha256 "c0883eb6e850a2d99eedf659c2851ad7a6535648ee1200de8d94174b05d48501"
 
   url "https://github.com/chartrandf/lookout/releases/download/v#{version}/Lookout_#{version}_universal.dmg"
